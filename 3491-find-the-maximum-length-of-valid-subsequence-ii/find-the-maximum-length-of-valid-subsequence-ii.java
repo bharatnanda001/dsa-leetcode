d7efdc810dc7b048c1120a80f1f34cc6fb119254
bharat nanda <143083253+bharatnanda001@users.noreply.github.com>
2025-07-17 15:56:43 +0530

    Time: 37 ms (70.10%) | Memory: 54 MB (40.21%) - LeetSync

---
+++ 3491-find-the-maximum-length-of-valid-subsequence-ii/find-the-maximum-length-of-valid-subsequence-ii.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maximumLength(int[] nums, int k) {
+        int[][] subarrayLengths = new int[k][k];
+        int maxLength = 0;
+
+        for (int num : nums) {
+            int modValue = ((num % k) + k) % k;  // Fix negative modulo
+            for (int j = 0; j < k; ++j) {
+                int requiredMod = (j - modValue + k) % k;
+                subarrayLengths[modValue][requiredMod] = subarrayLengths[requiredMod][modValue] + 1;
+                maxLength = Math.max(maxLength, subarrayLengths[modValue][requiredMod]);
+            }
+        }
+
+        return maxLength;
+    }
+}
