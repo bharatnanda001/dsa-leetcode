0c23c14ef33a178848874ed959d5b3bd6b65f744
bharat nanda <143083253+bharatnanda001@users.noreply.github.com>
2025-10-09 23:47:01 +0530

    Time: 103 ms (84.51%) | Memory: 44.3 MB (91.55%) - LeetSync

--- 3794-find-the-minimum-amount-of-time-to-brew-potions/find-the-minimum-amount-of-time-to-brew-potions.java
+++ 3794-find-the-minimum-amount-of-time-to-brew-potions/find-the-minimum-amount-of-time-to-brew-potions.java
@@ -2,13 +2,13 @@ import java.util.Arrays;
 
 class Solution {
     public long minTime(int[] skill, int[] mana) {
-        long sumSkill = Arrays.stream(skill).sum();          // sum of skills
-        long prevWizardDone = sumSkill * (long) mana[0];    // initial finish time after first potion
+        long sumSkill = Arrays.stream(skill).sum();         
+        long prevWizardDone = sumSkill * (long) mana[0];   
 
         for (int j = 1; j < mana.length; ++j) {
             long prevPotionDone = prevWizardDone;
             for (int i = skill.length - 2; i >= 0; --i) {
-                // remove the contribution of wizard (i+1) for potion (j-1)
+               
                 prevPotionDone -= (long) skill[i + 1] * mana[j - 1];
 
                 
