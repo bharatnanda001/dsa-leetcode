ad0844794ff664700adcd9294d1f973f38b2d996
bharat nanda <143083253+bharatnanda001@users.noreply.github.com>
2025-10-11 23:14:18 +0530

    Time: 264 ms (41.89%) | Memory: 68.1 MB (39.64%) - LeetSync

--- 3437-maximum-total-damage-with-spell-casting/maximum-total-damage-with-spell-casting.java
+++ 3437-maximum-total-damage-with-spell-casting/maximum-total-damage-with-spell-casting.java
@@ -1,16 +1,13 @@
 class Solution {
   public long maximumTotalDamage(int[] power) {
     Map<Integer, Integer> count = new HashMap<>();
-
     for (final int damage : power)
       count.merge(damage, 1, Integer::sum);
-
     List<Integer> uniqueDamages = getSortedUniqueDamages(count);
     final int n = uniqueDamages.size();
     // dp[i][k] := the maximum damage using uniqueDamages[0..i], where k
     // indicates if the i-th damage is used
     long[][] dp = new long[n][2];
-
     for (int i = 0; i < n; ++i) {
       final int damage = uniqueDamages.get(i);
       if (i == 0) {
@@ -29,10 +26,8 @@ class Solution {
         dp[i][1] += Math.max(dp[i - 3][0], dp[i - 3][1]);
       }
     }
-
     return Math.max(dp[n - 1][0], dp[n - 1][1]);
   }
-
   private List<Integer>getSortedUniqueDamages(Map<Integer, Integer> count) {
     List<Integer> uniqueDamages = new ArrayList<>(count.keySet());
     Collections.sort(uniqueDamages);
