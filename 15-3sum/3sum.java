1ac04b70db6ad8fc2c638e5b8622cbe1fcc7e60a
bharat nanda <143083253+bharatnanda001@users.noreply.github.com>
2025-12-10 09:56:12 +0530

    Time: 34 ms (45.62%) | Memory: 59.3 MB (19.46%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,38 +1,34 @@
 class Solution {
-    public List<List<Integer>> threeSum(int[] nums) {
-        Arrays.sort(nums);  // Change 'Array.sort' to 'Arrays.sort' (Java's sorting method)
+  public List<List<Integer>> threeSum(int[] nums) {
+    if (nums.length < 3)
+      return new ArrayList<>();
 
-        List<List<Integer>> result = new ArrayList<>();  // Fixed typo: 'ArraysList' → 'ArrayList'
-        
-        for(int i = 0; i < nums.length - 2; i++) {
-            if(i > 0 && nums[i] == nums[i - 1]) {
-                continue;  // Skip duplicate elements for i
-            }
-            
-            int left = i + 1;
-            int right = nums.length - 1;
-            
-            while(left < right) {  // You need this while loop to move left and right pointers
-                int sum = nums[i] + nums[left] + nums[right];
-                
-                if(sum == 0) {
-                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
-                    
-                    // Skip duplicates on left
-                    while(left < right && nums[left] == nums[left + 1]) left++;
-                    // Skip duplicates on right
-                    while(left < right && nums[right] == nums[right - 1]) right--;
-                    
-                    left++;
-                    right--;
-                } else if(sum < 0) {
-                    left++;  // If sum too small, increase left pointer
-                } else {
-                    right--;  // If sum too big, decrease right pointer
-                }
-            }
+    List<List<Integer>> ans = new ArrayList<>();
+
+    Arrays.sort(nums);
+
+    for (int i = 0; i + 2 < nums.length; ++i) {
+      if (i > 0 && nums[i] == nums[i - 1])
+        continue;
+
+      int l = i + 1;
+      int r = nums.length - 1;
+      while (l < r) {
+        final int sum = nums[i] + nums[l] + nums[r];
+        if (sum == 0) {
+          ans.add(Arrays.asList(nums[i], nums[l++], nums[r--]));
+          while (l < r && nums[l] == nums[l - 1])
+            ++l;
+          while (l < r && nums[r] == nums[r + 1])
+            --r;
+        } else if (sum < 0) {
+          ++l;
+        } else {
+          --r;
         }
-        
-        return result;  // Return the result list at the end of the function
+      }
     }
+
+    return ans;
+  }
 }
