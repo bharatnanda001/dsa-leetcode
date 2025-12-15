06bb41a7877020114923bbf48d5d8e7c8c9a9f18
bharat nanda <143083253+bharatnanda001@users.noreply.github.com>
2025-12-16 00:52:35 +0530

    Time: 2 ms (92.83%) | Memory: 44.5 MB (16.10%) - LeetSync

--- 1130-last-stone-weight-ii/last-stone-weight-ii.java
+++ 1130-last-stone-weight-ii/last-stone-weight-ii.java
@@ -1,25 +1,19 @@
-class Solution {
+class Solution {//sum = j 
+    int total;
+    Integer[][] memo;
+    int dfs(int[] stones , int i , int j ){
+        if(i ==stones.length)
+            return Math.abs(total-2*j);
+        if(memo[i][j] != null)
+            return memo[i][j];
+            int take = dfs(stones,i+1, j+stones[i]);
+            int skip = dfs(stones, i+1, j);
+            return memo[i][j]= Math.min(take,skip);
+    }
     public int lastStoneWeightII(int[] stones) {
-        int totalSum = 0;
-        for (int stone : stones) {
-            totalSum += stone;
-        }
-
-        int numStones = stones.length;
-        int targetCapacity = totalSum / 2;
-
-        int[][] dp = new int[numStones + 1][targetCapacity + 1];
-
-        for (int i = 1; i <= numStones; i++) {
-            for (int j = 0; j <= targetCapacity; j++) {
-                dp[i][j] = dp[i - 1][j];
-                if (stones[i - 1] <= j) {
-                    dp[i][j] = Math.max(dp[i][j], 
-                                        dp[i - 1][j - stones[i - 1]] + stones[i - 1]);
-                }
-            }
-        }
-
-        return totalSum - dp[numStones][targetCapacity] * 2;
+        total =0; 
+        for(int s :stones)total +=s;
+        memo  = new Integer[stones.length][total+1];
+        return dfs(stones,0,0);
     }
 }
