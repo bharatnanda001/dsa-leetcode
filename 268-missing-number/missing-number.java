5b97f6306077516475d8489ddf59df4a0d4f4940
bharat nanda <143083253+bharatnanda001@users.noreply.github.com>
2025-12-19 23:33:40 +0530

    Time: 0 ms (100.00%) | Memory: 47.1 MB (62.80%) - LeetSync

--- 268-missing-number/missing-number.java
+++ 268-missing-number/missing-number.java
@@ -1,17 +1,9 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        int missingN = 0;
+        int n = nums.length;
 
-        // XOR all numbers from 0 to n
-        for (int i = 0; i <= nums.length; i++) {
-            missingN ^= i;
-        }
-
-        // XOR with all elements of nums
-        for (int i = 0; i < nums.length; i++) {
-            missingN ^= nums[i];
-        }
-
-        return missingN;
+        int x  =n;
+        for(int i =0; i<n; i++) x^=i^nums[i];
+        return x;
     }
 }
