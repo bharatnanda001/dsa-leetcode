20e0860828dd0dab149ef950f448683052adddfa
bharat nanda <143083253+bharatnanda001@users.noreply.github.com>
2025-12-24 11:23:24 +0530

    Time: 18 ms (7.40%) | Memory: 122.5 MB (76.39%) - LeetSync

--- 334-increasing-triplet-subsequence/increasing-triplet-subsequence.java
+++ 334-increasing-triplet-subsequence/increasing-triplet-subsequence.java
@@ -1,22 +1,35 @@
 class Solution {
-    public boolean increasingTriplet(int[] nums) {
-        if (nums == null|| nums.length < 3) {
-            return false;
+    public Boolean increasingTriplet(int[] nums) {
+
+      ArrayList<Integer> lis = new ArrayList<>();
+      lis.add(nums[0]);
+
+      for(int i = 1 ; i< nums.length; i++){
+        if(nums[i]> lis.get(lis.size()-1)){
+            lis.add(nums[i]);
+        }else{
+            lis.set(LowerBound(lis,nums[i]),nums[i]);
         }
-        int a = Integer.MAX_VALUE;
-        int b = Integer.MAX_VALUE;
-        
-        for (int num : nums) {
-            if (num <= a) {
-                a = num;
-            } else if (num <= b) {
-                b = num;
-            } else {
-                return true;
-            }
+      }
+
+      return lis.size()>=3;
+    }
+    public int LowerBound(ArrayList<Integer> lis, int target) {
+    
+        int left = 0;
+        int right = lis.size()-1;
+
+        while(left <= right){
+           int mid = left +(right-left)/2;
+
+           if(lis.get(mid)== target){
+            return mid;
+           }else if( lis.get(mid)> target){
+            right = mid -1;
+           }else{
+            left = mid+1;
+           }
         }
-        
-        return false;
-        
+        return left;
     }
 }
