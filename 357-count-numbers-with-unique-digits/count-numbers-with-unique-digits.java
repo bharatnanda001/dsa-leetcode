1dc1fc489a82041de1272cdc2fbd75524618e150
bharat nanda <143083253+bharatnanda001@users.noreply.github.com>
2026-01-07 07:42:06 +0530

    Time: 2 ms (16.00%) | Memory: 43.1 MB (10.01%) - LeetSync

---
+++ 357-count-numbers-with-unique-digits/count-numbers-with-unique-digits.java
@@ -0,0 +1,53 @@
+import java.util.*;
+
+public class Solution {
+
+    static String limit;
+    static Integer[][][] dp;
+
+    public static int countNumbersWithUniqueDigits(int n) {
+        if (n == 0) return 1;
+
+        limit = String.valueOf((int) Math.pow(10, n) - 1);
+        int len = limit.length();
+        dp = new Integer[len][1 << 10][2];
+
+        return solve(0, 0, 1);
+    }
+
+    private static int solve(int pos, int mask, int tight) {
+
+        if (pos == limit.length()) {
+            return 1; // valid number formed
+        }
+
+        if (dp[pos][mask][tight] != null) {
+            return dp[pos][mask][tight];
+        }
+
+        int res = 0;
+        int ub = tight == 1 ? limit.charAt(pos) - '0' : 9;
+
+        for (int digit = 0; digit <= ub; digit++) {
+
+            int newTight = (tight == 1 && digit == ub) ? 1 : 0;
+
+            // leading zero → don't mark used
+            if (mask == 0 && digit == 0) {
+                res += solve(pos + 1, mask, newTight);
+            } 
+            // digit already used → skip
+            else if ((mask & (1 << digit)) != 0) {
+                continue;
+            } 
+            // use digit
+            else {
+                res += solve(pos + 1, mask | (1 << digit), newTight);
+            }
+        }
+
+        return dp[pos][mask][tight] = res;
+    }
+
+   
+}
