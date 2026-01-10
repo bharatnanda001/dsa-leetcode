59260105d16b47f7834475b59f235edba985619c
bharat nanda <143083253+bharatnanda001@users.noreply.github.com>
2026-01-11 00:11:47 +0530

    Time: 10 ms (99.60%) | Memory: 46.5 MB (45.28%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -1,23 +1,15 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        Integer[][] dp = new Integer[coins.length][amount + 1];
-        int ans = solve(coins.length - 1, amount, coins, dp);
-        return ans >= 1e9 ? -1 : ans;
-    }
+        int[] dp = new int[amount +1];
+        Arrays.fill(dp,amount+1);
+        dp[0]=0;
+        for(int coin : coins){
+            for(int i = coin; i<= amount; i++){
+                dp[i]= Math.min(dp[i], dp[i-coin]+1);
 
-    int solve(int i, int amt, int[] coins, Integer[][] dp) {
-        if (amt == 0) return 0;
-        if (i < 0) return (int)1e9;
-
-        if (dp[i][amt] != null) return dp[i][amt];
-
-        int notTake = solve(i - 1, amt, coins, dp);
-        int take = (int)1e9;
-
-        if (coins[i] <= amt)
-            take = 1 + solve(i, amt - coins[i], coins, dp);
-
-        return dp[i][amt] = Math.min(take, notTake);
+            }
+            
+        }
+        return dp[amount]>amount ?-1:dp[amount];
     }
 }
-
