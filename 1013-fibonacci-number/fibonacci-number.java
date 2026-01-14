48d6d21aaaa7f32cbfbdb7ef7ed22a2442ed64e7
bharat nanda <143083253+bharatnanda001@users.noreply.github.com>
2026-01-14 22:14:22 +0530

    Time: 2233 ms (9.06%) | Memory: 48.7 MB (8.39%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,11 +1,9 @@
 class Solution {
     public int fib(int n) {
-        //fiba(0) =, fib(1) = yeh base case ha 
-        //recussive case = fib(n)= fib(n-1)+ fib(n-2) yrh ha recrsibe case 
-        if( n==0) return 0;
-        if(n ==1) return 1;
-        return fib(n-1)+ fib(n-2);
-
+        int[] dp = new int[1000];
+        if(n<=1)return n;
+        if(dp[n]!= 0)return dp[n];
+        return dp[n]= fib(n-1)+fib(n-2);
         
     }
 }
