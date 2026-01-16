690661c2e61cbab2070e017d4a841f465e8005da
bharat nanda <143083253+bharatnanda001@users.noreply.github.com>
2026-01-17 00:04:28 +0530

    Time: 8 ms (84.36%) | Memory: 46 MB (73.51%) - LeetSync

--- 326-power-of-three/power-of-three.java
+++ 326-power-of-three/power-of-three.java
@@ -1,6 +1,8 @@
 class Solution {
     public boolean isPowerOfThree(int n) {
-        //1162261467 is 3^19 and 3^20 is bigger then int
-        return n>0 && 1162261467 %n==0;        
+        if(n<=0)return false;
+        while(n %3==0)n = n/3;
+        return n ==1;
+        
     }
 }
