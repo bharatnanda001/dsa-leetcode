93390487379eddd661fa6bc02e630f95027c203c
bharat nanda <143083253+bharatnanda001@users.noreply.github.com>
2026-01-18 12:18:10 +0530

    Time: 0 ms (100.00%) | Memory: 48.4 MB (6.33%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -1,23 +1,18 @@
 class Solution {
     public double myPow(double x, int n) {
-        long N = n;
-        if(N < 0){
+        long N =n;
+        if(N<0){
             x =1/x;
-            N = -N;
-        } 
-        return fastPow(x, N);
-        
-    }
-    private double fastPow(double x , long n){
-        if(n ==0)return 1;
-        double half= fastPow(x, n/2);
-        if(n%2==0){
-            return half*half;
-
+            N=-N;
         }
-        else{
-            return half* half*x;
+        double ans =1.0;
+        while(N>0){
+            if(N%2==1){
+                ans *=x;
+            }
+            x *=x;
+            N /=2;
         }
-
+        return ans;
     }
 }
