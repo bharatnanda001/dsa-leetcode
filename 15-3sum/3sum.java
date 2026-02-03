9b7fdd18c603f8d809882508cd04d415614e0e01
bharat nanda <143083253+bharatnanda001@users.noreply.github.com>
2026-02-03 23:37:03 +0530

    Time: 34 ms (59.04%) | Memory: 60.2 MB (14.56%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,34 +1,26 @@
 class Solution {
-  public List<List<Integer>> threeSum(int[] nums) {
-    if (nums.length < 3)
-      return new ArrayList<>();
-
-    List<List<Integer>> ans = new ArrayList<>();
-
-    Arrays.sort(nums);
-
-    for (int i = 0; i + 2 < nums.length; ++i) {
-      if (i > 0 && nums[i] == nums[i - 1])
-        continue;
-
-      int l = i + 1;
-      int r = nums.length - 1;
-      while (l < r) {
-        final int sum = nums[i] + nums[l] + nums[r];
-        if (sum == 0) {
-          ans.add(Arrays.asList(nums[i], nums[l++], nums[r--]));
-          while (l < r && nums[l] == nums[l - 1])
-            ++l;
-          while (l < r && nums[r] == nums[r + 1])
-            --r;
-        } else if (sum < 0) {
-          ++l;
-        } else {
-          --r;
+    public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> res = new LinkedList<>();
+        for(int i = 0; i<nums.length-2; i++){
+            if (i ==0 ||i >0 && nums[i] != nums[i-1]){
+                int low = i +1,high = nums.length-1, sum =0- nums[i];
+                while(low < high ){
+                    if(nums[low]+nums[high]==sum){
+                        res.add(Arrays.asList(nums[i], nums[low],nums[high]));
+                        while(low<high && nums[low]== nums[low+1])low++;
+                        while(low<high && nums[high]== nums[high-1])high--;
+                        low++;
+                        high--;
+                    }else if(nums[low]+nums[high]<sum){
+                        low++;
+                    }else{
+                        high--;
+                    }
+                }
+            }
+            
         }
-      }
+        return res;
     }
-
-    return ans;
-  }
 }
