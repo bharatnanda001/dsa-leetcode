0bd8a4e5540a1d29c86b36140c74b51d0f504351
bharat nanda <143083253+bharatnanda001@users.noreply.github.com>
2026-04-27 23:26:54 +0530

    Time: 295 ms (50.00%) | Memory: 28.3 MB (37.50%) - LeetSync

---
+++ 1507-check-if-there-is-a-valid-path-in-a-grid/check-if-there-is-a-valid-path-in-a-grid.py
@@ -0,0 +1,37 @@
+from collections import deque
+
+class Solution:
+    def hasValidPath(self, grid):
+        m, n = len(grid), len(grid[0])
+        
+        dirs = {
+            1: [(0, -1), (0, 1)],
+            2: [(-1, 0), (1, 0)],
+            3: [(0, -1), (1, 0)],
+            4: [(0, 1), (1, 0)],
+            5: [(0, -1), (-1, 0)],
+            6: [(0, 1), (-1, 0)]
+        }
+        
+        q = deque([(0, 0)])
+        visited = set([(0, 0)])
+        
+        while q:
+            r, c = q.popleft()
+            
+            if (r, c) == (m - 1, n - 1):
+                return True
+            
+            for dx, dy in dirs[grid[r][c]]:
+                nr, nc = r + dx, c + dy
+                
+                if 0 <= nr < m and 0 <= nc < n and (nr, nc) not in visited:
+                    
+                    # check reverse connection
+                    for bdx, bdy in dirs[grid[nr][nc]]:
+                        if nr + bdx == r and nc + bdy == c:
+                            visited.add((nr, nc))
+                            q.append((nr, nc))
+                            break
+        
+        return False
